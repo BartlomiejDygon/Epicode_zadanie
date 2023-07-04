2547b58d0154e3eaaf7459b546023694c0c0d242
DygonDev <136848344+DygonDev@users.noreply.github.com>
2023-07-04 16:47:27 +0200

    add edit user

--- src/Service/FileService.php
+++ src/Service/FileService.php
@@ -1,19 +1,22 @@
 <?php
 
 namespace App\Service;
+
 use App\Entity\File;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
-class UserService extends AbstractController
+class FileService extends AbstractController
 {
 	public function __construct(
 		private readonly SluggerInterface $slugger
 	) {
 	}
-	public function setFile($file) {
-		$fileToSave = new File();
+	public function setFile($file, $fileToSave) {
+		if(!$fileToSave) {
+			$fileToSave = new File();
+		}
 		$fileToSave->setSize($file->getSize());
 		$fileToSave->setOriginalName($file->getClientOriginalName());
 		$originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
@@ -26,10 +29,21 @@ class UserService extends AbstractController
 			);
 			$fileToSave->setClearName($newFilename);
 		} catch (FileException $e) {
-				// ... handle exception if something happens during file upload
+			// ... handle exception if something happens during file upload
 		}
 
 		return $fileToSave;
+	}
+
+	public function removeFile($file) {
+
+		$filePath = $this->getParameter('user_cv_directory') .'/'. $file->getClearName();
+		if (!file_exists($filePath)) {
+			throw $this->createNotFoundException('Plik nie istnieje.');
+		}
+		unlink($filePath);
+
+		return true;
 	}
 
 }
